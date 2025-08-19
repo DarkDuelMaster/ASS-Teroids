73c34f0cb07039491685647906580b49a17168f0
DarkDuelMaster <amaurycanales@gmail.com>
2025-08-18 21:15:48 -0600

    added rotation to the triangle

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
         			return
+		dt = clock.tick(60)/1000
 		screen.fill("black")
+		player1.update(dt)
 		player1.draw(screen)
 		pygame.display.flip()
-		dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -18,4 +18,14 @@ class Player(CircleShape):
 	def draw(self, screen):
 		pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
+	def rotate(self, dt):
+		player_rotation = (PLAYER_TURN_SPEED * dt)
+		self.rotation += player_rotation
 
+	def update(self, dt):
+        	keys = pygame.key.get_pressed()
+
+        	if keys[pygame.K_a]:
+            		self.rotate(-1*dt)
+        	if keys[pygame.K_d]:
+            		self.rotate(dt)
